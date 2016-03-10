8de466d63aa04ec42d6ea0f1a67e96ef03335698
Alpheaus Feltham <afeltham@mail.uoguelph.ca>
2016-03-10 16:44:55 -0500

    ****

--- A2/include/fio.h
+++ A2/include/fio.h
@@ -1,30 +1,30 @@
-	#ifndef _AFELTHAM_HEADERH
+#ifndef _AFELTHAM_HEADERH
 #define _AFELTHAM_HEADERH
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct Burst {
+typedef struct BurstInfo {
     int burstNum;
     int CPUTime;
     int IOTime;
     Burst * nextBurst;
-};
+}Burst;
 
-typedef struct Thread {
+typedef struct ThreadInfo {
     int	threadNum;
     int arrivalTime;
     int numBursts;
     Thread * nextThread;
     Burst * nextBurst;
-};
+}Thread;
 
-typedef struct Process {
+typedef struct ProcessInfo {
     int processNum;
     int numThreads;
     Process * nextProcess;
     Thread * nextThread;
-};
+}Process;
 
 void parse(FILE * input);
 
