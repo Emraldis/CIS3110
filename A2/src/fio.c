7bc1215ef5d57ee68f6f067c4da2f520a342f8d2
Emraldis <alfie.feltham@gmail.com>
2016-03-12 21:14:01 -0500

    fix var name

--- A2/src/fio.c
+++ A2/src/fio.c
@@ -76,7 +76,7 @@ Burst * createBurst(char * input, int processNum){
 	newBurst->CPUTime = atoi(tempString);
 	tempString = strtok(NULL," ");
 	newBurst->readyTime = -1;
-	newBurst->remaingTime = CPUTime;
+	newBurst->remainingTime = CPUTime;
 	if(tempString != NULL){
 		newBurst->IOTime = atoi(tempString);
 	}else{
