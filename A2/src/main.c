d3e0ec61f4c7cd89b73db6fa5125faf7960f20a7
Emraldis <alfie.feltham@gmail.com>
2016-03-12 22:00:06 -0500

    test

--- A2/src/main.c
+++ A2/src/main.c
@@ -160,5 +160,39 @@ int main (int argc, char * argv[]){
 		}
 		tick++;
 	}
+        printf("\nNumber of Processes: %d\n Overhead between Threads of the same Process: %d\nOverhead between Threads of different Processes: %d\n",processNum,processTrans,threadTrans);
+		i = 1;
+		while(i != 0){
+			printf("\nProcess Number: %d\nNumber of Threads: %d\n",currentProcess->processNum,currentProcess->numThreads);
+			currentThread = currentProcess->nextThread;
+			k = 1;
+			while(k != 0){
+				printf("\n\tThread Number: %d\n\t Arrival Time: %d \n\tNumber of Bursts: %d\n",currentThread->threadNum,currentThread->arrivalTime,currentThread->numBursts);
+				currentBurst = currentThread->nextBurst;
+				j = 1;
+				while(j != 0){
+					printf("\n\t\tBurst Number: %d\n\t\tCPU Time: %d\n\t\tIO Time: %d\n",currentBurst->burstNum, currentBurst->CPUTime, currentBurst->IOTime);
+					if(currentBurst->nextBurst != NULL){
+						currentBurst = currentBurst->nextBurst;
+					}else{
+						j = 0;
+					}
+					//getchar();
+				}
+				if(currentThread->nextThread != NULL){
+					currentThread = currentThread->nextThread;
+				}else{
+					k = 0;
+				}
+			}
+			if(currentProcess->nextProcess != NULL){
+				currentProcess = currentProcess->nextProcess;
+			}else{
+				i = 0;
+			}
+		}
+	//}
+	currentProcess = ProcessList;
+
     return(0);
 }
