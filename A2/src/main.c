02f38d9d3069ea74dca9f22ddccb084a6ddfa7fc
Emraldis <alfie.feltham@gmail.com>
2016-03-13 14:50:07 -0400

    debugging time!

--- A2/src/main.c
+++ A2/src/main.c
@@ -227,7 +227,7 @@ int main (int argc, char * argv[]){
 			}
 		}
 		if((debug == 3) && (tick % 10 == 0)){
-			//getchar();
+			getchar();
 			if(CPU != NULL){
 				printf("\n\tBurst in CPU:\n\tProcess: %d\tThread: %d\tBurst: %d\n",CPU->processNum,CPU->threadNum,CPU->nextBurst->burstNum);
 				printf("\tCurrent remaining CPU time: %d\n",CPU->nextBurst->remainingTime);
