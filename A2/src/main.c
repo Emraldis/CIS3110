8025e0e6cb74080e27db7fe3c08b2fc55a4b7e7a
Emraldis <alfie.feltham@gmail.com>
2016-03-13 22:20:42 -0400

    got rid of unnecessary output

--- A2/src/main.c
+++ A2/src/main.c
@@ -98,7 +98,7 @@ int main (int argc, char * argv[]){
 	List Debug Code:
 	***************************************************************************************************************************************/
 	
-	//if(debug == 1){
+	if(debug == 1){
         printf("\nNumber of Processes: %d\n Overhead between Threads of the same Process: %d\nOverhead between Threads of different Processes: %d\n",processNum,processTrans,threadTrans);
 		i = 1;
 		while(i != 0){
@@ -129,7 +129,7 @@ int main (int argc, char * argv[]){
 				i = 0;
 			}
 		}
-	//}
+	}
 	currentProcess = ProcessList;
 
 	
