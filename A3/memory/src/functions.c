79390d21584904e02cc10e8fdc2176a999707451
Emraldis <alfie.feltham@gmail.com>
2016-03-25 19:24:16 -0400

    stupid pointers being stupid

--- A3/memory/src/functions.c
+++ A3/memory/src/functions.c
@@ -13,11 +13,7 @@ Process * lineParse(char * input){
 	
 	
 	if (tempString != NULL){
-		printf("\ntest");
-		getchar();
 		strcpy(newProcess->label,tempString);
-		printf("\nfinished");
-		getchar();
 	}
 	
 	tempString = strtok(NULL,"\0");
