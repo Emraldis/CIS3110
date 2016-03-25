b8c875582f11b9d0e0b7327c0d61384fe4fba08b
Emraldis <alfie.feltham@gmail.com>
2016-03-25 17:42:24 -0400

    time for le bugtesting!

--- A3/memory/src/main.c
+++ A3/memory/src/main.c
@@ -20,13 +20,19 @@ int main (int argc, char * argv[]) {
 	
 	inputFile = fopen(fileName, "r");
 	
+	printf("\nFile opened\n");
+	getchar();
+	
 	while(fgets(input,256,inputFile) != NULL){
 		disk = addToBack(disk,lineParse(input));
+		printf("\nParsed a line\n");
+		getchar();
 	}
 	currentProcess = disk->prcList;
 	while(currentProcess != NULL){
 		printf("\nName: %s\nSize: %d\n",currentProcess->label,currentProcess->memoryReq);
 		currentProcess = currentProcess->next;
+		getchar();
 	}
 	
 	return(0);
