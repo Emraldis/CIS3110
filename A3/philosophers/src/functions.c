e0f137b14700f1d7b4d0132ad3a17f19c4d022b7
Emraldis <alfie.feltham@gmail.com>
2016-03-26 18:07:34 -0400

    fixes

--- A3/philosophers/src/functions.c
+++ A3/philosophers/src/functions.c
@@ -2,13 +2,18 @@
 
 void * eat(void * id, void * timesEat){
 	int i;
+	int phlID
+	int loop;
 	
-	for(i=0;i<timesEat;i++){
-		printf("\nPhilosopher %d is thinking.",id);
+	phlID = (int) id;
+	loop = (int) timesEat;
+	
+	for(i=0;i<loop;i++){
+		printf("\nPhilosopher %d is thinking.",phlID);
 		sem_wait(&semaphr);
-		printf("\nPhilosopher %d is thinking.",id);
+		printf("\nPhilosopher %d is thinking.",phlID);
 		sem_post(&semaphr);
-		printf("\nPhilosopher %d is thinking.",id);
+		printf("\nPhilosopher %d is thinking.",phlID);
 	}
 	
 	pthread_exit();
