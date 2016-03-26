7fbd8d7bc10c6e2996a92f07cf7137a4c6c4ccf3
Emraldis <alfie.feltham@gmail.com>
2016-03-26 18:26:57 -0400

    more changes

--- A3/philosophers/include/functions.h
+++ A3/philosophers/include/functions.h
@@ -1,7 +1,7 @@
 #include<semaphore.h>
 #include<pthread.h>
 #include<stdio.h>
-#inlcude<stdlibl.h>
+#inlcude<stdlib.h>
 #include<string.h>
 
 void * eat(void * data[2]);
--- A3/philosophers/src/functions.c
+++ A3/philosophers/src/functions.c
@@ -4,9 +4,12 @@ void * eat(void * data[2]){
 	int i;
 	int phlID;
 	int loop;
+	char * input;
 	
-	phlID = atoi((char *) strtok(data," "));
-	loop = atoi((char *) data);
+	input = (char *) data;
+	
+	phlID = atoi(strtok(input," "));
+	loop = atoi(input);
 	
 	for(i=0;i<loop;i++){
 		printf("\nPhilosopher %d is thinking.",phlID);
