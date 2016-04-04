189532585e4c970732d8ac180339dd3177ac6cff
Emraldis <alfie.feltham@gmail.com>
2016-04-04 01:03:44 -0400

    quick update

--- A4/src/main.c
+++ A4/src/main.c
@@ -27,10 +27,10 @@ int main (void){
 		timeStart = start.tv_sec + (double)start.tv_usec / 1000000.0;
 		timeEnd = end.tv_sec + (double)end.tv_usec / 1000000.0;
 		difference = timeEnd-timeStart;
+		sprintf(outputString,"Start time: %f End time: %f Time elapsed: %f",timeStart, timeEnd, difference);
 		outputFile = fopen("output.txt","a");
-		fputs("");
+		fputs(outputString,outputFile);
 		fclose(outputFile);
-		printf("Start time: %f End time: %f Time elapsed: %f",timeStart, timeEnd, difference);
 		if(i<1000000){
 			i = (i + 100);	
 			temp = realloc(temp,(sizeof(char) * i));
