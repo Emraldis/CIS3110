4fd77eea94a7cb9f0734c1034bcc2100bb194ef0
Emraldis <alfie.feltham@gmail.com>
2016-04-05 20:57:52 -0400

    added secondary output mode

--- A4/src/main.c
+++ A4/src/main.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 #include <sys/time.h>
 
-int main (void){
+int main (int argc, char * argv[]){
 	int i;
 	char * temp;
 	FILE * inputFile;
@@ -13,8 +13,12 @@ int main (void){
 	struct timeval start;
 	struct timeval end;
 	char * outputString;
+	int a;
+	int j;
 
+	a = atoi(argv[1]);
 	i = 1000;
+	j = 0;
 	outputString = malloc(sizeof(char) * 256);
 	temp = malloc(sizeof(char) * i);
 	outputFile = fopen("size.txt","w");
@@ -24,37 +28,54 @@ int main (void){
 	outputFile = fopen("end.txt","w");
 	fclose(outputFile);
 	do{
-		inputFile = fopen("testfile.txt","r");
-		gettimeofday(&start, NULL);
-		fgets(temp,i,inputFile);
-		printf("\nread %d bytes",i);
-		gettimeofday(&end, NULL);
-		fclose(inputFile);
-		timeStart = start.tv_sec + (double)start.tv_usec / 1000000.0;
-		timeEnd = end.tv_sec + (double)end.tv_usec / 1000000.0;
-		difference = timeEnd-timeStart;
-		sprintf(outputString,"%d\n",i);
-		outputFile = fopen("size.txt","a");
-		fputs(outputString,outputFile);
-		fclose(outputFile);
-		sprintf(outputString,"%f\n",timeStart);
-		outputFile = fopen("start.txt","a");
-		fputs(outputString,outputFile);
-		fclose(outputFile);
-		sprintf(outputString,"%f\n",timeEnd);
-		outputFile = fopen("end.txt","a");
-		fputs(outputString,outputFile);
-		fclose(outputFile);
-		sprintf(outputString,"%f\n",difference);
-		outputFile = fopen("difference.txt","a");
-		fputs(outputString,outputFile);
-		fclose(outputFile);
-		if(i<1000000){
-			i = (i + 1000);	
-			temp = realloc(temp,(sizeof(char) * i));
-			//getchar();
+		if(a == 1){
+			inputFile = fopen("testfile.txt","r");
+			gettimeofday(&start, NULL);
+			fgets(temp,i,inputFile);
+			printf("\nread %d bytes",i);
+			gettimeofday(&end, NULL);
+			fclose(inputFile);
+			timeStart = start.tv_sec + (double)start.tv_usec / 1000000.0;
+			timeEnd = end.tv_sec + (double)end.tv_usec / 1000000.0;
+			difference = timeEnd-timeStart;
+			sprintf(outputString,"%d\n",i);
+			outputFile = fopen("size.txt","a");
+			fputs(outputString,outputFile);
+			fclose(outputFile);
+			sprintf(outputString,"%f\n",difference);
+			outputFile = fopen("difference.txt","a");
+			fputs(outputString,outputFile);
+			fclose(outputFile);
+			if(i<1000000){
+				i = (i + 1000);	
+				temp = realloc(temp,(sizeof(char) * i));
+				//getchar();
+			}else{
+				break;
+			}
+		}else if(a == 2){
+			j++;
+			gettimeofday(&start, NULL);
+			inputFile = fopen("testfile.txt","r");
+			gettimeofday(&end, NULL);
+			fclose(inputFile);
+			timeStart = start.tv_sec + (double)start.tv_usec / 1000000.0;
+			timeEnd = end.tv_sec + (double)end.tv_usec / 1000000.0;
+			difference = timeEnd-timeStart;
+			sprintf(outputString,"%d\n",j);
+			outputFile = fopen("occurence.txt","a");
+			fputs(outputString,outputFile);
+			fclose(outputFile);
+			sprintf(outputString,"%f\n",difference);
+			outputFile = fopen("difference2.txt","a");
+			fputs(outputString,outputFile);
+			fclose(outputFile);
+			if(j >= 100){
+				break;
+			}
 		}else{
-			break;
+			printf("\nError encountered, Quitting\n");
+			return(0);
 		}
 	}while(temp != NULL);
 	printf("\n");
